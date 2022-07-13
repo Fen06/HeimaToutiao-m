894b285169a30d9a5cb0268fe217ca39b563c918
18God <1226343906@qq.com>
2022-07-13 18:08:59 +0800

    第一天内容完成

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,6 @@
 <template>
   <div>
-    <h1>黑马头条</h1>
-    <!-- <span class="toutiao toutiao-dianzan"></span> -->
-    <span class="toutiao toutiao-dianzan"></span>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-
-    <div class="dl"></div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -21,11 +12,4 @@ export default {
 }
 </script>
 
-<style scoped>
-.dl {
-  width: 694px;
-  height: 88px;
-  background-color: #6db4fb;
-  border-radius: 10px;
-}
-</style>
+<style scoped></style>
---
+++ src/views/Login/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <div>
+    <!-- 头部区域 -->
+    <van-nav-bar class="navbar" title="登录" @click-left="base">
+      <!-- @click-left="onClickLeft" -->
+      <template #left>
+        <van-icon name="cross" />
+      </template>
+    </van-nav-bar>
+    <!-- 头部区域 -->
+    <!-- 表单区域 -->
+    <van-form @submit="login" class="from">
+      <van-field
+        v-model="mobile"
+        name="mobile"
+        placeholder="手机号"
+        :rules="[{ required: true, message: '请填写手机号' }]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-shouji"></span>
+        </template>
+      </van-field>
+      <van-field
+        v-model="code"
+        type="text"
+        name="code"
+        placeholder="验证码"
+        :rules="[{ required: true, message: '请填写验证码' }]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-yanzhengma"></span>
+        </template>
+        <template #right-icon>
+          <van-button class="cobe-btn" size="mini" round
+            >发送验证码</van-button
+          ></template
+        >
+      </van-field>
+      <div style="margin: 16px">
+        <van-button block type="info" native-type="submit">提交</van-button>
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+// import request from '@/utils'
+// import { Toast } from 'vant'
+import { login } from '@/api/user'
+export default {
+  name: 'Login',
+  data () {
+    return {
+      mobile: '',
+      code: ''
+    }
+  },
+
+  methods: {
+    base () {
+      this.$router.base()
+    },
+
+    async login () {
+      const res = await login(this.mobile, this.code)
+      console.log(res)
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.navbar {
+  background-color: #3296fa;
+  :deep(.van-nav-bar__title) {
+    color: #fff;
+  }
+  .van-icon {
+    color: #fff;
+  }
+}
+
+.from {
+  :deep(.van-field__label) {
+    flex: 1;
+  }
+  :deep(.van-cell__value) {
+    flex: 20;
+  }
+
+  .toutiao {
+    font-size: 0.46333rem;
+  }
+  .cobe-btn {
+    padding: 0 0.33333rem;
+    color: #666;
+  }
+}
+</style>
