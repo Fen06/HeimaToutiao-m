5b4f982e1adadab051b7365cae5c34c8048e8071
18God <1226343906@qq.com>
2022-07-16 19:10:46 +0800

    home

---
+++ src/views/Home/component/AtricleList.vue
@@ -0,0 +1,16 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: [String, Object],
+      required: true
+    }
+  }
+}
+</script>
+
+<style></style>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,126 @@
 <template>
-  <div>Home</div>
+  <div>
+    <van-nav-bar class="navbar">
+      <template #title>
+        <van-button round>
+          <van-icon name="search" />
+          搜索
+        </van-button>
+      </template>
+    </van-nav-bar>
+
+    <van-tabs v-model="active" swipeable>
+      <van-tab v-for="item in channel" :key="item.id" :title="item.name">
+        <AtricleList :id="item.id"></AtricleList>
+      </van-tab>
+      <span class="toutiao toutiao-gengduo"></span>
+    </van-tabs>
+  </div>
 </template>
 
 <script>
-export default {}
+import { getMyChannels } from '@/api'
+import AtricleList from '@/views/Home/component/AtricleList.vue'
+export default {
+  name: 'home',
+  data () {
+    return {
+      active: 2,
+      channel: []
+    }
+  },
+  components: {
+    AtricleList
+  },
+
+  created () {
+    this.getMyChannels()
+  },
+
+  methods: {
+    async getMyChannels () {
+      try {
+        const { data } = await getMyChannels()
+        this.channel = data.data.channels
+        console.log(this.channel)
+      } catch (error) {
+        this.$toast.fail('数据请求失败，请重试')
+      }
+    }
+  }
+}
 </script>
 
-<style></style>
+<style lang="less" scoped>
+//tabs选项卡
+:deep(.van-tabs__wrap) {
+  padding-right: 66px;
+
+  .van-tabs__nav {
+    padding-left: 0;
+    padding-right: 0;
+
+    /* tab标签 */
+    .van-tab {
+      border: 1px solid #eee;
+      width: 200px;
+      height: 82px;
+    }
+
+    /* tab标签下划线 */
+    .van-tabs__line {
+      width: 31px;
+      height: 6px;
+      background-color: #3296fa;
+      bottom: 40px;
+    }
+  }
+}
+
+/* 字体图标 */
+.toutiao-gengduo {
+  position: absolute;
+  top: 0;
+  right: 0;
+  width: 66px;
+  height: 82px;
+  font-size: 40px;
+  line-height: 82px;
+  text-align: center;
+  opacity: 0.6;
+  border-bottom: 1px solid #eee;
+
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 50%;
+    transform: translateY(-50%);
+    height: 70%;
+    width: 1px;
+    background-image: url('~@/assets/images/gradient-gray-line.png');
+  }
+}
+
+.navbar {
+  background-color: #3296fa;
+  color: #fff;
+  .van-button {
+    width: 555px;
+    height: 64px;
+    background-color: #5babfb;
+  }
+  :deep(.van-nav-bar__title) {
+    max-width: unset;
+  }
+  .van-button__text {
+    color: #fff;
+  }
+  .van-icon-search {
+    color: #fff;
+  }
+  .van-button--default {
+    border: 0.02667rem solid #5babfb;
+  }
+}
+</style>
